90103d786ae056f22a56a6c3ee60c32def860d4f
JaySanders95 <jaysanders1878@gmail.com>
2023-09-24 14:59:04 +0000

    added list view for bookings + added tuple display for human readable name

---
+++ .history/bookings/models_20230924140927.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Table (models.Model):
+    
+    table_number = models.PositiveIntegerField(unique=True)
+    capacity = models.PositiveIntegerField()
+    is_available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"Table{self.table_number}"
+
+class Booking(models.Model):
+
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    table = models.ForeignKey(Table, on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.IntegerField(choices=TIME_CHOICES, default=1)
+    num_guests = models.PositiveIntegerField()
+    notes = models.TextField(max_length=40, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def get_time_display(self):
+        """Get the display value of the selected time."""
+        time_choices_display = dict(TIME_CHOICES)
+        return time_choices_display.get(self.time, 'Unknown')
+
+    def __str__(self):
+        return f" Covers: {self.num_guests} Date: {self.date}"
---
+++ .history/bookings/views_20230923234130.py
@@ -0,0 +1,72 @@
+from django import forms
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from bookings.models import Booking, Table
+from bookings.forms import BookingForm
+
+class BookingCreateView(LoginRequiredMixin, CreateView):
+    template_name = 'bookings/booking_add.html'
+    form_class = BookingForm
+    model = Booking
+
+    def form_valid(self, form):
+        form.instance.customer = self.request.user
+
+        capacity = form.cleaned_data ['num_guests']
+        date = form.cleaned_data['date']
+        time = form.cleaned_data['time']
+
+        table = self.find_free_table(capacity, date, time)
+        
+
+        if table:
+            booking = Booking.objects.create(
+                table=table,
+                date=date,
+                time=time,
+                customer=self.request.user,
+                num_guests = capacity
+            )
+            booking.save()
+            messages.success(self.request, "Booking completed successfully!")
+            return redirect('booking_add')
+        else:
+            messages.error(self.request, "Your booking could not be made, please try a different time.")
+            return render(self.request, self.template_name, {'form' : form})
+
+    def find_free_table(self, capacity, date, time):
+        booking_num = int(capacity)
+        if booking_num % 2 == 1:
+            booking_num += 1
+        
+        capacity = str(booking_num)
+        # Retreive all available tables
+        available_tables = Table.objects.filter(capacity=capacity, is_available=True)
+
+        for table in available_tables:
+            #check availability for specified date and time
+            existing_booking = Booking.objects.filter(table=table, date=date, time=time).first()
+            if not existing_booking:
+                return table
+            
+        return None
+    
+    def get_queryset(self):
+        return Table.objects.filter(is_available=True)
+
+
+class BookingListView(LoginRequiredMixin, ListView):
+    template_name = 'bookings/my_bookings.html'
+    model = Booking
+    context_object_name = 'bookings'
+
+    def get_queryset(self):
+        #Queryset function for managing booking search
+        user = self.request.user
+
+        # If user is staff, shows all bookings
+        if user.is_staff:
+            return Booking.objects
