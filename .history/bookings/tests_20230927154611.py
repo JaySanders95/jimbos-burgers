91f09624533fa621d5f85919806e0d995a56db59
JaySanders95 <jaysanders1878@gmail.com>
2023-09-27 20:52:57 +0000

    finished automated testing

---
+++ .history/bookings/tests_20230927154611.py
@@ -0,0 +1,41 @@
+from django.test import TestCase
+from bookings.models import Booking, Table
+from django.contrib.auth.models import User
+from datetime import date
+
+class TableTests(TestCase):
+    
+    def setUp(self):
+        #Creates a table for testing
+        self.table = Table.objects.create(table_number=1, capacity=4, is_available=True)
+
+    def test_table_str_representation(self):
+        #Test string representation
+        self.assertEqual(str(self.table), "Table1")
+    
+    def test_table_fields(self):
+        #TEst table fields
+        self.assertEqual(self.table.table_number, 1)
+        self.assertEqual(self.table.capacity, 4)
+        self.assertEqual(self.table.is_available, True)
+
+    def test_table_unique_constraint(self):
+        #Test unique constraint on table_number
+        with self.assertRaises(Exception):
+            Table.objects.create(table_number=1, capacity=2, is_available=True)
+        
+class BookingTest(TestCase):
+    #Creates a user, table and booking for table
+    self.user = User.objects.create(username="testuser", password="testpassword")
+    self.table = Table.objects.create(table_number=1, capacity=4, is_available=True)
+    self.booking = Booking.objects.create(
+        customer=self.user,
+        table=self.table,
+        date=date(2023, 11, 12),
+        time= 3,
+        num_guests=3,
+        notes="Notes"
+    )
+
+    expected_str = "Covers: 3 Date: 2023-10-15"
+    self.assertEqual(str(self.booking), expected expected_str)
---
+++ .history/jimbo_burger/urls_20230927163634.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from django.urls import path, include
+from home.views import home, menu, CustomSignupView
+from bookings.views import BookingCreateView, BookingListView, BookingUpdateView, BookingDeleteView
+from allauth.account.views import SignupView
+from django.conf import settings
+
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', home, name="home"),
+    path('menu/', menu, name="menu"),
+    path('accounts/signup/', CustomSignupView.as_view(), name='custom_signup'),
+    path("accounts/", include("allauth.urls")),
+    path('bookings/', BookingListView.as_view(), name='my_bookings'),
+    path('bookings/add/', BookingCreateView.as_view(), name='booking_add'),
+    path('bookings/<int:pk>/update/', BookingUpdateView.as_view(), name='booking_edit'),
+    path('bookings/<int:pk>/delete/', BookingDeleteView.as_view(), name='booking_defensive'),
+
+]
