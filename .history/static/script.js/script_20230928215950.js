62748f3004936f111d8e76ee532f11b454083ac5
JaySanders95 <jaysanders1878@gmail.com>
2023-09-28 22:01:32 +0000

    styling changes / edit booking changes

---
+++ .history/static/script.js/script_20230928215950.js
@@ -0,0 +1,16 @@
+setTimeout (function() {
+    let messages = document.getElementById("msg");
+    let alert = new bootstrap.Alert(messages);
+    alert.close();
+}, 3000);
+
+
+function checkDate() {
+    var selectedDate = new Date(document.getElementById('id_time').value);
+    var now = new Date();
+
+    if (selectedDate < now) {
+    alert("Date must be in the future");
+    window.location.reload();
+    }
+}
